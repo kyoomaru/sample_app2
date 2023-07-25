92608b6524d647d9a2de335c1d5af8249f5e588a
kyoomaru <138374899+kyoomaru@users.noreply.github.com>
2023-07-25 03:03:05 +0000

    sample_app[chapter4]

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,29 @@
+# モデルなどの処理はコントローラに書くこと！
+class ListsController < ApplicationController
+  def new
+    @list = List.new
+    # List.newの意味はリストモデルの情報をもとにnew（新規作成）するって意味だよ！
+  end
+
+  def create
+    list = List.new(list_params)
+    list.save
+    redirect_to list_path(list.id)
+  end
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  private
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
